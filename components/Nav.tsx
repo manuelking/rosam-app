9b18cc0e7845f46a679fc80fe434437e97d848dc
manuelking <emmanuel_dunyo@outlook.com>
2023-11-25 17:45:14 +0000

    customised quote button

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from 'react-device-detect'
 
 function Nav() {
   return (
@@ -16,15 +22,25 @@ function Nav() {
         </Link>
       </div>
       <div className="flex-1 flex justify-end">
-        <Link
-          href="https://contact.brighterteam.co.uk/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="bg-sun text-cobalt font-semibold xxs:text-[18px] text-[14px] py-2 xs:px-10 px-4 rounded-full">
-            Get a quote
-          </button>
-        </Link>
+        <BrowserView>
+          <Link
+            href="https://contact.brighterteam.co.uk/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="bg-sun text-cobalt font-semibold xxs:text-[18px] text-[14px] py-2 xs:px-10 px-4 rounded-full">
+              Get a quote
+            </button>
+          </Link>
+        </BrowserView>
+        <MobileView>
+          <Link href="tel:01138730647">
+            <button className="bg-sun text-cobalt font-semibold xxs:text-[18px] text-[14px] py-2 xs:px-7 px-4 rounded-full flex flex-row items-center gap-2">
+              <Image src="/phone.svg" alt="phone icon" width={18} height={18} />
+              <p>Get a quote</p>
+            </button>
+          </Link>
+        </MobileView>
       </div>
     </div>
   )
