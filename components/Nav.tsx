9f67dd684f2deb59883d2f411c4700e7356eb9f9
manuelking <emmanuel_dunyo@outlook.com>
2023-11-25 18:39:27 +0000

    conditionally rendering button based on device

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,7 @@
+// 'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   BrowserView,
   MobileView,
@@ -9,6 +10,16 @@ import {
 } from 'react-device-detect'
 
 function Nav() {
+  // const [isOnMobile, setIsOnMobile] = useState(false)
+
+  // console.log(isOnMobile)
+
+  // useEffect(() => {
+  //   if (!isMobile) {
+  //     setIsOnMobile(true)
+  //   }
+  // }, [])
+
   return (
     <div className="sticky top-0 w-screen py-[24px] ss:px-24 xxs:px-10 px-4 bg-white shadow-lg flex justify-between items-center z-10">
       <div className="flex-1 flex justify-start">
@@ -22,7 +33,14 @@ function Nav() {
         </Link>
       </div>
       <div className="flex-1 flex justify-end">
-        <BrowserView>
+        {isMobile ? (
+          <Link href="tel:01138730647">
+            <button className="bg-sun text-cobalt font-semibold xxs:text-[18px] text-[14px] py-2 xs:px-7 px-4 rounded-full flex flex-row items-center gap-2">
+              <Image src="/phone.svg" alt="phone icon" width={18} height={18} />
+              <p>Get a quote</p>
+            </button>
+          </Link>
+        ) : (
           <Link
             href="https://contact.brighterteam.co.uk/"
             target="_blank"
@@ -32,15 +50,7 @@ function Nav() {
               Get a quote
             </button>
           </Link>
-        </BrowserView>
-        <MobileView>
-          <Link href="tel:01138730647">
-            <button className="bg-sun text-cobalt font-semibold xxs:text-[18px] text-[14px] py-2 xs:px-7 px-4 rounded-full flex flex-row items-center gap-2">
-              <Image src="/phone.svg" alt="phone icon" width={18} height={18} />
-              <p>Get a quote</p>
-            </button>
-          </Link>
-        </MobileView>
+        )}
       </div>
     </div>
   )
