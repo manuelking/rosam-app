6e1c431fc41e06287fadf77ccf54ec798b1deee8
manuelking <emmanuel_dunyo@outlook.com>
2023-11-25 18:45:37 +0000

    conditionally rendering button based on isOnMobile state

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
@@ -10,15 +10,15 @@ import {
 } from 'react-device-detect'
 
 function Nav() {
-  // const [isOnMobile, setIsOnMobile] = useState(false)
+  const [isOnMobile, setIsOnMobile] = useState(false)
 
-  // console.log(isOnMobile)
+  console.log(isOnMobile)
 
-  // useEffect(() => {
-  //   if (!isMobile) {
-  //     setIsOnMobile(true)
-  //   }
-  // }, [])
+  useEffect(() => {
+    if (!BrowserView) {
+      setIsOnMobile(true)
+    }
+  }, [])
 
   return (
     <div className="sticky top-0 w-screen py-[24px] ss:px-24 xxs:px-10 px-4 bg-white shadow-lg flex justify-between items-center z-10">
@@ -33,7 +33,7 @@ function Nav() {
         </Link>
       </div>
       <div className="flex-1 flex justify-end">
-        {isMobile ? (
+        {isOnMobile ? (
           <Link href="tel:01138730647">
             <button className="bg-sun text-cobalt font-semibold xxs:text-[18px] text-[14px] py-2 xs:px-7 px-4 rounded-full flex flex-row items-center gap-2">
               <Image src="/phone.svg" alt="phone icon" width={18} height={18} />
